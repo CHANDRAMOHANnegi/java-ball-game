34b881e23a5e67a4b090055000d711b7e0bf5a8a
cm <cmsingh1721997@gmail.com>
2018-08-04 22:46:18 +0530

    Add files via upload

---
+++ Game1Constants.java
@@ -0,0 +1,23 @@
+
+public interface Game1Constants {
+	
+	static  final int WIDTH1 =900;
+	  
+	static  final int HEIGHT1=700;
+	
+
+	static  final int PLATE_WIDTH1 =100;
+	  
+	static  final int PLATE_HEIGHT1=10;
+	
+	final int DELAY=50;
+	  final int BALLWIDTH1=50;
+	  final int BALLHEIGHT1=50;
+      final String TITLE="GAME 1 ";
+	
+      final String BACKGROUND_IMAGE1="bg1.jpg";
+  	static  final int MAXDRAGONBALLS=10;
+  	
+	
+
+}
